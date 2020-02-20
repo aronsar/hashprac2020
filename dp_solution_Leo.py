5520fc83517cceab964a3e395afb35ec72738861
Yi-Chen Liu <55065942+leo19900723@users.noreply.github.com>
2020-02-19 20:51:59 -0800

    Update dp_solution_Leo.py

--- dp_solution_Leo.py
+++ dp_solution_Leo.py
@@ -1,4 +1,5 @@
 import math
+from PracticeProblem import utils
 
 
 def print2D(maze):
@@ -9,26 +10,29 @@ def print2D(maze):
 
 
 def knapsackProblem(pizzas, maxSlices):
-    dp = [[0 for _ in range(maxSlices)] for _ in range(len(pizzas) + 1)]
+    dp = [[0 for _ in range(maxSlices + 1)] for _ in range(len(pizzas) + 1)]
+    orderDP = [[set() for _ in range(maxSlices + 1)] for _ in range(len(pizzas) + 1)]
 
     for numOfPizza in range(len(pizzas) + 1):
-        for slice in range(maxSlices):
+        for slice in range(maxSlices + 1):
             if numOfPizza == 0:
                 dp[numOfPizza][slice] = 0
             else:
-                dp[numOfPizza][slice] = max((dp[numOfPizza - 1][slice - pizzas[numOfPizza - 1]] + pizzas[numOfPizza - 1]) if slice >= pizzas[numOfPizza - 1] else -math.inf, dp[numOfPizza - 1][slice])
-        print2D(dp)
+                if (dp[numOfPizza - 1][slice - pizzas[numOfPizza - 1]] + pizzas[numOfPizza - 1]) if slice >= pizzas[numOfPizza - 1] else -math.inf > dp[numOfPizza - 1][slice]:
+                    dp[numOfPizza][slice] = (dp[numOfPizza - 1][slice - pizzas[numOfPizza - 1]] + pizzas[numOfPizza - 1])
+                    orderDP[numOfPizza][slice] = orderDP[numOfPizza][slice].union(orderDP[numOfPizza - 1][slice - pizzas[numOfPizza - 1]])
+                    orderDP[numOfPizza][slice].add(numOfPizza - 1)
+                else:
+                    dp[numOfPizza][slice] = dp[numOfPizza - 1][slice]
+                    orderDP[numOfPizza][slice] = orderDP[numOfPizza][slice].union(orderDP[numOfPizza - 1][slice])
 
-    return dp[-1][-1]
+    return len(orderDP[-1][-1]), orderDP[-1][-1]
 
 
 def main():
 
-    with open("a_example.in", "r") as inputFile:
-        input = inputFile.read().splitlines()
+    maxSlices, types, pizzas = utils.load_input_file("c_medium.in")
 
-    maxSlices, types = map(int, input[0].split(" "))
-    pizzas = list(map(int, input[1].split(" ")))
     print(knapsackProblem(pizzas, maxSlices))
 
     return
