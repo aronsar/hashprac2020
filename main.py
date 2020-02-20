b289ebc3f80c2bf9ed205828b1b56bd60990468c
aronsar <aronsar@ad3.ucdavis.edu@interaction.cs.ucdavis.edu>
2020-02-19 18:17:29 -0800

    setting up framework

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
+import argparse 
+import utils
+from dp_solution import dp_solution
+from hybrid_solution import hybrid_solution
+
+
+parser = argparse.ArgumentParser() 
+parser.add_argument(
+    '--input_file', 
+    type=str, 
+    default='input/c_medium.in', 
+    help='relative path of input file') 
+parser.add_argument(
+    '--dp_limit', 
+    type=int, 
+    default=1000, 
+    help='the max size of the dynamic programming table') 
+args = parser.parse_args()
 
 
 if __name__ == '__main__':
-    pass
+    target, total_pizzas, slice_arr = utils.load_input_file(args.input_file)
+
+    if target * total_pizzas < args.dp_limit:
+        idx_arr = dp_solution(slice_arr, target)
+
+    else:
+        idx_arr = hybrid_solution(slice_arr, target)
+
+    utils.write_output_file(idx_arr)
---
+++ utils.py
@@ -0,0 +1,6 @@
+def load_input_file(input_file_path):
+    pass
+
+
+def write_output_file(idx_array):
+    pass
